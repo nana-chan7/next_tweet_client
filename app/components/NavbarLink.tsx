d7b3ff2cd761c201e5d859c53014e9ff44931264
frogBear_7 <{220098tt@yse-c.net}>
2024-01-17 10:58:52 +0900

    up

---
+++ app/components/NavbarLink.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+interface NavbarLinkProps {
+    href: string;
+    label: string;
+    onClick?: () => void; // ?=任意
+}
+
+const NavbarLink = ({href, label, onClick} : NavbarLinkProps) => {
+    return (
+        <Link 
+            href={href}
+            className='hidden md:inline-block p-3 text-black'
+            onClick={onClick}
+            >
+            {label}
+        </Link>
+    );
+}
+
+export default NavbarLink;
