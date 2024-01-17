2dcbbd2cb7b45dd7fd2167e63222e4ee1edb216d
frogBear_7 <{220098tt@yse-c.net}>
2024-01-17 11:44:18 +0900

    up

---
+++ app/auth/regist/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { FaUser } from "react-icons/fa";
+
+const RegistPage = () => { // nafe: 入力    
+    return (
+        <div className="mx-auto w-1/3">
+            <h1 className="flex text-2xl text-center font-bold">
+                <FaUser className="mt-1 me-3" />                
+                Register
+            </h1>
+        </div>
+    );
+}
+
+export default RegistPage;
+
